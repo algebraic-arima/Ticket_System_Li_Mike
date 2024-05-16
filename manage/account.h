2dfe4f25e3e3b1269faedc96ceae4bf1a7767278
algebraic-arima <lfk.alg@gmail.com>
2024-05-16 22:59:32 +0800

    login: std::map try storing id->(pos,privilege)

--- manage/account.h
+++ manage/account.h
@@ -2,6 +2,7 @@
 #define TICKET_SYSTEM_LI_MIKE_ACCOUNT_H
 
 #include "BlockRiver.h"
+#include <map>
 
 namespace arima_kana {
 
@@ -34,11 +35,10 @@ namespace arima_kana {
       std::fstream acc_filer;
       size_t num = 0;
       BlockRiver<acc_id, size_t, 80> list;
-      BlockRiver<acc_id, short, 80> priv_list;
-      vector<acc_id> login_list;
+      std::map<acc_id, pair<size_t, short>> login_list;
 
       explicit Account(const std::string &af = "account") :
-              list(af), priv_list(af + "_priv") {
+              list(af) {
         acc_file = af + "_raw";
         acc_filer.open(acc_file, std::ios::in);
         if (!acc_filer.is_open()) {
@@ -69,81 +69,157 @@ namespace arima_kana {
         acc_filer.close();
       }
 
+      void read_main(AccountInfo &t, const size_t pos) {
+        acc_filer.open(acc_file, std::ios::in | std::ios::binary);
+        acc_filer.seekg((pos - 1) * sizeof(AccountInfo) + sizeof(size_t));
+        acc_filer.read(reinterpret_cast<char *>(&t), sizeof(AccountInfo));
+        acc_filer.close();
+      }
+
+      void write_main(const AccountInfo &t, const size_t pos) {
+        acc_filer.open(acc_file, std::ios::in | std::ios::out | std::ios::binary);
+        acc_filer.seekp((pos - 1) * sizeof(AccountInfo) + sizeof(size_t));
+        acc_filer.write(reinterpret_cast<const char *>(&t), sizeof(AccountInfo));
+        acc_filer.close();
+      }
+
       bool add_usr(const acc_id &c_id, const AccountInfo &usr) {
         if (num == 0) {//no usr
           AccountInfo root = usr;
           root.priv = 10;
           list.insert(usr.id, ++num);
-          priv_list.insert(usr.id, 10);
           append_main(root);
           return true;
         }
-        if (!login_list.find(c_id)) return false;//not logged in
-        auto pos = priv_list.find(c_id);
-        if (pos.size() != 1) return false;//not found or duplicated
-        if (pos[0] <= usr.priv) return false;//privilege not enough
-        if (!list.find(usr.id).empty()) return false;//exists
+        auto it = login_list.find(c_id);
+        if (it == login_list.end())
+          return false; //not logged in
+        if (it->second.second <= usr.priv)
+          return false; //privilege not enough
+        if (!list.find(usr.id).empty())
+          return false;//exists
         list.insert(usr.id, ++num);
-        priv_list.insert(usr.id, usr.priv);
         append_main(usr);
         return true;
       }
 
       bool login(const acc_id &id, const passwd &pw) {
         auto pos = list.find(id);
-        if (pos.size() != 1) return false;
-        if (login_list.find(id)) return false;
-        acc_filer.open(acc_file, std::ios::in | std::ios::binary);
-        acc_filer.seekg((pos[0] - 1) * sizeof(AccountInfo) + sizeof(size_t));
+        if (pos.size() != 1)
+          return false;//not found or duplicated
+        if (login_list.find(id) != login_list.end())
+          return false;
         AccountInfo tmp;
-        acc_filer.read(reinterpret_cast<char *>(&tmp), sizeof(AccountInfo));
-        acc_filer.close();
-        if (tmp.pw != pw) return false;
-        login_list.push_back(id);
+        read_main(tmp, pos[0]);
+        if (tmp.pw != pw)
+          return false;
+        login_list[id] = pair(pos[0], tmp.priv);
         return true;
       }
 
       bool logout(const acc_id &id) {
-        if (!login_list.find(id)) return false;
-        login_list.erase(id);
+        auto it = login_list.find(id);
+        if (it == login_list.end())
+          return false;
+        login_list.erase(it);
         return true;
       }
 
       AccountInfo get_usr(const acc_id &c_id, const acc_id &id) {
-        if (!login_list.find(c_id)) {
+        auto it = login_list.find(c_id);
+        if (it == login_list.end()) {
           error("not logged in");
         }
         auto pos = list.find(id);
         if (pos.size() != 1) {
           error("User not found or duplicated");
         }
-        acc_filer.open(acc_file, std::ios::in | std::ios::binary);
-        acc_filer.seekg((pos[0] - 1) * sizeof(AccountInfo) + sizeof(size_t));
         AccountInfo tmp;
-        acc_filer.read(reinterpret_cast<char *>(&tmp), sizeof(AccountInfo));
-        acc_filer.close();
-        auto priv_pos = priv_list.find(c_id);
-        if (c_id != id && priv_list.find(c_id)[0] <= tmp.priv) {
+        read_main(tmp, pos[0]);
+        if (c_id != id && it->second.second <= tmp.priv) {
           error("privilege not enough");
         }
         return tmp;
       }
 
-      bool modify_usr(const acc_id &id, const passwd &pw) {
+      AccountInfo modify_usr(const acc_id &c_id, const acc_id &id, const passwd &pw) {
+        auto it = login_list.find(c_id);
+        if (it == login_list.end()) {
+          error("not logged in");
+        }
         auto pos = list.find(id);
         if (pos.size() != 1) {
-          return false;
           error("User not found or duplicated");
         }
-        acc_filer.open(acc_file, std::ios::out | std::ios::in | std::ios::binary);
-        acc_filer.seekg((pos[0] - 1) * sizeof(AccountInfo) + sizeof(size_t));
         AccountInfo tmp;
-        acc_filer.read(reinterpret_cast<char *>(&tmp), sizeof(AccountInfo));
-        acc_filer.seekp((pos[0] - 1) * sizeof(AccountInfo) + sizeof(size_t));
+        read_main(tmp, pos[0]);
+        if (c_id != id && it->second.second <= tmp.priv) {
+          error("privilege not enough");
+        }
         tmp.pw = pw;
-        acc_filer.write(reinterpret_cast<const char *>(&tmp), sizeof(AccountInfo));
-        acc_filer.close();
-        return true;
+        write_main(tmp, pos[0]);
+        return tmp;
+      }
+
+      AccountInfo modify_usr(const acc_id &c_id, const acc_id &id, const name &nm) {
+        auto it = login_list.find(c_id);
+        if (it == login_list.end()) {
+          error("not logged in");
+        }
+        auto pos = list.find(id);
+        if (pos.size() != 1) {
+          error("User not found or duplicated");
+        }
+        AccountInfo tmp;
+        read_main(tmp, pos[0]);
+        if (c_id != id && it->second.second <= tmp.priv) {
+          error("privilege not enough");
+        }
+        tmp.nm = nm;
+        write_main(tmp, pos[0]);
+        return tmp;
+      }
+
+      AccountInfo modify_usr(const acc_id &c_id, const acc_id &id, const mailaddr &mail) {
+        auto it = login_list.find(c_id);
+        if (it == login_list.end()) {
+          error("not logged in");
+        }
+        auto pos = list.find(id);
+        if (pos.size() != 1) {
+          error("User not found or duplicated");
+        }
+        AccountInfo tmp;
+        read_main(tmp, pos[0]);
+        if (c_id != id && it->second.second <= tmp.priv) {
+          error("privilege not enough");
+        }
+        tmp.mail = mail;
+        write_main(tmp, pos[0]);
+        return tmp;
+      }
+
+      AccountInfo modify_usr(const acc_id &c_id, const acc_id &id, const short &priv) {
+        auto it = login_list.find(c_id);
+        if (it == login_list.end()) {
+          error("not logged in");
+        }
+        auto pos = list.find(id);
+        if (pos.size() != 1) {
+          error("User not found or duplicated");
+        }
+        AccountInfo tmp;
+        read_main(tmp, pos[0]);
+        if (c_id != id && it->second.second <= tmp.priv) {
+          error("privilege not enough");
+        }
+        if (it->second.second <= priv) {
+          error("modify: privilege not enough");
+        }
+        tmp.priv = priv;
+        write_main(tmp, pos[0]);
+        it->second.second = priv;
+        return tmp;
       }
     };
 
---
+++ manage/train.h
@@ -0,0 +1,16 @@
+#ifndef TICKET_SYSTEM_TRAIN_H
+#define TICKET_SYSTEM_TRAIN_H
+
+#include "utility.h"
+
+namespace arima_kana {
+
+    typedef m_string<22> train_id;
+    typedef m_string<34> station_name;
+
+    class TrainInfo {
+
+    };
+}
+
+#endif
