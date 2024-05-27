c6b5ee635a2cbdf96481156da0064e3b21dca1f6
algebraic-arima <lfk.alg@gmail.com>
2024-05-27 22:36:53 +0800

    no progress in TLE and MLE

--- src/account.h
+++ src/account.h
@@ -41,7 +41,7 @@ namespace arima_kana {
 
     class Account {
     public:
-      unique_BlockRiver<acc_id, AccountInfo, 32, 200, 90, 100> list;
+      unique_BlockRiver<acc_id, AccountInfo, 32, 200, 90, 60> list;
       std::map<acc_id, short> login_list;
 
       explicit Account(const std::string &af = "1account") :
