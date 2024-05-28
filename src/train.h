661faced2a21d57601ad58ffb25b3825312b58fd
algebraic-arima <lfk.alg@gmail.com>
2024-05-28 14:28:17 +0800

    train no buffer edge stores the main info

--- src/train.h
+++ src/train.h
@@ -253,8 +253,8 @@ namespace arima_kana {
 
     class Train {
     public:
-      unique_BlockRiver<train_id, TrainInfo, 16, 128, 48, 60> train_list;
-      BlockRiver<station_name, EdgeInfo, 22, 22, 10, 100> edge_list;
+      unique_BlockRiver<train_id, TrainInfo, 16, 128, 48, 1> train_list;
+      BlockRiver<station_name, EdgeInfo, 22, 22, 10, 1000> edge_list;
       unique_BlockRiver<pair<station_name, train_id>, EdgeInfo, 56, 20, 8, 60> station_train_to_ind;
       Seat seat_list;
       // insert into train_list and seat_list to initialize
