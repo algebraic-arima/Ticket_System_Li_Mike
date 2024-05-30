35f8433773c90ba1e5e756803d6eb739597448a4
algebraic-arima <lfk.alg@gmail.com>
2024-05-30 20:46:54 +0800

    param adjusted

--- src/account.h
+++ src/account.h
@@ -41,7 +41,7 @@ namespace arima_kana {
 
     class Account {
     public:
-      unique_BlockRiver<acc_id, AccountInfo, 32, 180, 88, 50, 50> list;
+      unique_BlockRiver<acc_id, AccountInfo, 32, 180, 88, 30, 30> list;
       //tree: 22*180*100=400kb
       //data: (22+104)*32*100=400kb
       map<acc_id, short> login_list;
